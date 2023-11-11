6130c6bc3d7d65fe299d9d67d5d1b5761c35e841
torcuata22 <marilynjmarquez@gmail.com>
2023-11-11 11:45:00 -0500

    new repo

---
+++ src/App.styles.ts
@@ -0,0 +1,70 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import BGImage from '/Users/marilynmarquez/Documents/GitHub/quiz/src/images/maxim-berg.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    font-weight:bold;
+    margin: 0;
+  }
+
+  h1 {
+    font-family: Fascinate Inline;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    font-weight: 400;
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(1px 1px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+  }
+
+  .start, .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #ffffff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0);
+    border-radius: 25px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+
+  .start {
+    max-width: 200px;
+  }
+`;
